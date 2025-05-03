99e937be7b758cfed6adf95bf8302a69462f8636
TouHouE <hsu.william9@gmail.com>
2025-05-03 20:42:01 +0800

    update: upload finetune model scripts

---
+++ scripts/vqa_lora_prompt.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+deepspeed src/train/train_vlm.py \
+    --deepspeed ./scripts/zero2.json \
+    --wb_name PromptMed3DVLM-Qwen-2.5-7B-lora \
+    --vision_tower "prompt_dcformer" \
+    --model_name_or_path Qwen/Qwen2.5-7B-Instruct \
+    --model_type vlm_qwen \
+    --pretrain_vision_model ./output/DCFormer_SigLIP/pretrained_ViT.bin \
+    --mm_projector_type "mixer" \
+    --lora_enable True \
+    --vision_select_layer -2 \
+    --pretrain_mllm /home/jovyan/workspace/Med3DVLM/models/VLM \
+    --data_root ./data \
+    --bf16 True \
+    --output_dir ./output/PromptMed3DVLM-Qwen-2.5-7B-lora \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --eval_strategy "no" \
+    --eval_accumulation_steps 1 \
+    --eval_steps 0.04 \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 1 \
+    --learning_rate 5e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 0.001 \
+    --gradient_checkpointing False \
+    --dataloader_pin_memory True \
+    --dataloader_num_workers 4

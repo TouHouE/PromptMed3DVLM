f7f180711d316bd363717789406d30555bc10b6f
TouHouE <hsu.william9@gmail.com>
2025-06-16 08:59:04 +0800

    fix: wrong action `strore_true`

--- src/dataset/utils/transforms.py
+++ src/dataset/utils/transforms.py
@@ -6,7 +6,7 @@ from monai import transforms as MT
 
 
 def nnunet_scaler(pack) -> dict | Iterable[float | int]:
-    is_dict = isinstance(pack, dict)        
+    is_dict = isinstance(pack, dict)
     is_torch = torch.is_tensor(pack['image']) if is_dict else torch.is_tensor(pack)
 
     cliper = torch.clip if is_torch else np.clip
@@ -18,7 +18,7 @@ def nnunet_scaler(pack) -> dict | Iterable[float | int]:
         cliper(pack, -395.0, 842.0, out=pack)
         pack -= 279.8117370605469
         pack /= 253.5583953857422
-    
+
     return pack
 
 
@@ -102,19 +102,19 @@ def select_fn(x):
 
 
 class DummyCropForeground(MT.Transform):
-    def __init__(self, classes_range: range | list[int, int], source_key: str, **kwargs):
+    def __init__(self, classes_range: range | list[int], source_key: str, **kwargs):
         """
             The classes_range is the range of the foreground classes if is a list the actual range will be:
             list(n0, n1) -> range(n0, n1 + 1)
         """
         # super().__init__(**kwargs)
         if isinstance(classes_range, list):
-            classes_range = range(classes_range[0], classes_range[1] + 1)                        
+            classes_range = range(classes_range[0], classes_range[1] + 1)
         self.cropper = MT.CropForegroundd(select_fn=select_fn, source_key=source_key, **kwargs)
         self.rand_cropper = MT.CenterSpatialCrop(roi_size=(128, 128, 128))
         self.source_key = source_key
         self.full_class = classes_range
-        
+
     def __call__(self, pack: dict):
         src_k = self.source_key
 
@@ -138,20 +138,20 @@ class MixedResizer(MT.Transform):
     def __init__(self, keys, allow_missing_keys, spatial_size, padder_kwargs, resizer_kwargs):
         self.final_size = spatial_size
         self.padder = MT.ResizeWithPadOrCropd(
-            keys=keys, allow_missing_keys=allow_missing_keys, 
+            keys=keys, allow_missing_keys=allow_missing_keys,
             spatial_size=self.final_size, **padder_kwargs
         )
         if resizer_kwargs.get('size_mode') == 'longest' and not isinstance(spatial_size, int):
             spatial_size = max(spatial_size)
         self.resizer = MT.Resized(
-            keys=keys, allow_missing_keys=allow_missing_keys, 
+            keys=keys, allow_missing_keys=allow_missing_keys,
             spatial_size=spatial_size, **resizer_kwargs
         )
-    
-    def __call__(self, pack):        
+
+    def __call__(self, pack):
         pack = self.resizer(pack)
         return self.padder(pack)
-    
+
 
 class PseudoJPEGScaleIntensity:
     def __init__(self, keys, tensor_type: Literal['numpy', 'torch']):

75716f45af4e4f3e382761a22734380698ef2789
uc207pr4f57t9 <hsu.william9@gmail.com>
2025-06-20 08:48:42 +0000

    feat: making the training scripts to fix 2 GPU

--- scripts/siglip/prompt_siglip.sh
+++ scripts/siglip/prompt_siglip.sh
@@ -6,6 +6,7 @@ export pretrained_model="/home/jovyan/shared/uc207pr4f57t9/cardiac/model/dcforme
 export data_root="/home/jovyan/shared/uc207pr4f57t9/cardiac/taipei/taipei"
 export cur_model_name=CardiacSigLIP_Prompt_nnunet_E500
 export output_dir="/home/jovyan/shared/uc207pr4f57t9/cardiac/RunOutput/SigLIP/$cur_model_name"
+export append_keys_cfg_path=/home/jovyan/shared/uc207pr4f57t9/cardiac/code/PromptMed3DVLM/config/append_keys.json
 export eval_output_dir=/home/jovyan/shared/uc207pr4f57t9/cardiac/RunOutput/eval/SigLIP/$cur_model_name
 # export output_dir="/home/jovyan/shared/uc207pr4f57t9/cardiac/RunOutput/SigLIP/debug"
 
@@ -18,29 +19,34 @@ deepspeed src/train/train_clip.py \
     --loss_type "sigmoid" \
     --data_root $data_root \
     --max_length 512 \
-    --append_keys image_fg;image_fgs \
+    --append_keys $append_keys_cfg_path \
     --loader_type unet-med3d-fgcrop \
     --bf16 True \
     --output_dir $output_dir \
     --num_train_epochs 500 \
-    --per_device_train_batch_size 64 \
+    --per_device_train_batch_size 16 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --eval_strategy "no" \
     --eval_accumulation_steps 1 \
     --eval_steps 0.04 \
     --save_strategy "steps" \
-    --save_steps 200 \
-    --save_total_limit 2 \
-    --learning_rate 1e-4 \
+    --save_steps 100 \
+    --save_total_limit 5 \
+    --learning_rate 2.5e-5 \
     --weight_decay 0.1 \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
-    --logging_steps 0.001 \
+    --logging_steps 0.00001 \
     --gradient_checkpointing False \
     --dataloader_pin_memory True \
-    --dataloader_num_workers 8
+    --dataloader_num_workers 4
+# Setting 1 For 8 gpus
+# bs: 64, lr: 1e-4
+# Setting 2 For 2 gpus
+# bs: 16, lr: 2.5e-5
 
+# Old logging_steps 0.001
 #python src/eval/cardiac_eval_clip.py \
 #    --model_name_or_path $output_dir \
 #    --data_root $data_root \
